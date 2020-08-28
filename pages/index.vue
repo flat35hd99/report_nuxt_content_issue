43203168870affc8d067c26d9d041e3d507b9278
flat35hd99 <flat35hd99@gmail.com>
2020-08-29 00:14:43 +0900

    initial commit.

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul>
+      <li
+        v-for="document in documents"
+        :key="document.slug"
+      >
+        <nuxt-link :to="document.path">{{ document.title }}</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content }) {
+    const documents = await $content({ deep: true }).fetch()
+    return {
+      documents
+    }
+  }
+}
+</script>
